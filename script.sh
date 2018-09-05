318fb2c6f2b87864b599e19356ef6ab4ef94fed2
wintoo <wintoo143@gmail.com>
2018-09-04 23:51:56 -0500

    script.sh edited

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
 # executing rsync command to deploy the code on webserver.
-rsync -avz /var/lib/jenkins/workspace/test/index.html wintoo@192.168.61.99:/var/www/html/
+rsync -avz Host_key_verification=no /var/lib/jenkins/workspace/test/index.html wintoo@192.168.61.99:/var/www/html/
