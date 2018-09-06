444991143859ea1ecc973ddfe6bee20d3df5e5c2
wintoo <wintoo143@gmail.com>
2018-09-06 16:07:20 -0500

    Removed Readme.md and script.sh files from repo

--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash
-
-# executing rsync command to deploy the code on webserver.
-rsync -avz Host_key_verification=no /var/lib/jenkins/workspace/test/index.html wintoo@192.168.61.99:/var/www/html/
