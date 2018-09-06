8a53851a1839c32698c63cebeb5a5f203930ce4e
wintoo <wintoo143@gmail.com>
2018-09-06 18:06:05 -0500

    removing script.sh file

--- script.sh
+++
@@ -1,5 +0,0 @@
-# push docker images to docker hub after build.
-docker build -t wintoo143/httpd:v1
-
-# create a docker container from created custom image.
-docker container run -tdi --name webserver wintoo143/httpd:v1
